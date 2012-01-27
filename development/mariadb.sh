c08a3d978b39ee878dc6496bb7aacf37bb7ae06a
Ecko <jmartin@ohaiworld.net>
2012-01-27 02:25:23 +0000

    MariaDB Install

---
+++ development/mariadb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash/
+
+install_mariadb()
+{
+    echo -n "Building dependancies for Mariadb..."
+    apt-get update #> /dev/null 2>&1
+    apt-get build-deb mysql-server -y #> /dev/null 2>&1
+    mkdir ~/repos && cd ~/repos
+    echo "done."
+    sleep 5
+    echo -n "Downloading MariaDB..."
+    wget http://downloads.askmonty.org/f/mariadb-5.2.10/kvm-tarbake-jaunty-x86/mariadb-5.2.10.tar.gz/from/http:/ftp.osuosl.org/pub/mariadb #> /dev/null 2>&1
+    echo "done."
+    mv mariadb mariadb.tar.gz && tar -zxf mariadb.tar.gz && rm -rf maria.tar.gz
+    mv mariadb* mariadb && cd mariadb
+    sleep 5
+    echo -n "Stage 1: Building install for MariaDB. This may take some time..."
+    BUILD/compile-pentium64-max #> /dev/null 2>&1
+    echo "done"
+    sleep 5
+    echo -n "Stage 2: Making the install.  This may take some time..."
+    make install #> /dev/null 2>&1
+    echo "done."
+}
